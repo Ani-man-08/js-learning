c19ccf572088e3b261889980c7c4c718bef1eb06
Aniruddha Mankar <aniruddhmankar110@gmail.com>
2026-01-29 17:16:00 +0000

    commit and push

--- 04_iterators/seven.js
+++ 04_iterators/seven.js
@@ -4,5 +4,5 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 const result = myNumbers
                     .map((item) => item * 10)
                     .map((item) => item + 1)
-                    .filter((item) => item > 50)
+                    .filter((item) => item > 40)
 console.log(result)
