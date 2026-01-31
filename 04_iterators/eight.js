7fb82ef022f27e2d5263a8ab2c958a0dced10773
Aniruddha Mankar <aniruddhmankar110@gmail.com>
2026-01-31 17:45:48 +0000

    ei

--- 04_iterators/eight.js
+++ 04_iterators/eight.js
@@ -1,6 +1,27 @@
 const myNums = [1,2,3,4,5]
 
-const myTotal = myNums.reduce(function(acc,currValue){
-    return acc + currValue
-})
+// const myTotal = myNums.reduce(function(acc,currValue){
+//     console.log(`Accumulator: ${acc} | Current Value: ${currValue}`)
+//     return acc + currValue;
+// },0)
+
+const myTotal = myNums.reduce((acc,currValue) => acc + currValue,0)
 console.log(myTotal)
+
+const shoppingCart = [
+    {
+        itemName: "Laptop",
+        price: 30000
+    },
+    {
+        itemName: "TV",
+        price: 20000
+    },
+    {
+        itemName: "Fridge",
+        price: 10000
+    },
+]
+
+const cartTotal = shoppingCart.reduce((acc, currItem) => acc + currItem.price,0)
+console.log(cartTotal)
